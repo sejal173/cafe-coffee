496d33861dab352243097d525a5e08fa41e64780
sejal173 <sejalkakade123@gmail.com>
2026-08-29 17:44:27 +0530

    created about page

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -3,25 +3,36 @@ import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import BodyContainer from "../../components/BodyContainer/BodyContainer";
 import PageTitle from "../../components/PageTitle/PageTitle";
+import img from './../../assets/coffe-shop-img.jpg'
+
 
 function About(){
     return(
        <div>
         <Navbar active="about"/>
             <BodyContainer>
-                <PageTitle title="This is about page"/>
-                <PageTitle subtitle="We believe a coffee shop is more than just a place to drink coffee. It is a place to relax, meet friends, work, and enjoy your favorite flavors."/>
+                <PageTitle title="More Than Just Food, It’s an Experience!"/>
+                <PageTitle subtitle="We believe great food brings people together. From fresh ingredients to delicious flavors, every dish is prepared with care to give you a memorable dining experience."/>
                 
 
-                 <div className="main-containt-container">
-          Our goal is to provide high-quality coffee made from carefully
-          selected beans. Whether you love a classic cappuccino, a creamy latte,
-          or a strong espresso, we have something for every coffee lover. We
-          believe a coffee shop is more than just a place to drink coffee. It is
-          a place to relax, meet friends, work, and enjoy your favorite flavors.
-          Come visit us and enjoy a fresh cup of happiness! ☕
-        </div>
-                
+                <div className="main-container">
+
+                    <div>
+            <img src={img} alt="" />
+                    </div>
+
+                    <div className="story-content">
+                        <h1>Our Story</h1>
+                        <b>Our journey started with a simple love for great coffee, delicious food, and good conversations. We wanted to create a cozy place where people could relax, enjoy their favorite flavors, and spend quality time with friends and family.
+
+From freshly brewed coffee to tasty meals, every item on our menu is prepared with fresh ingredients and lots of care. We believe that good food is not just about taste — it is about creating happy moments and memorable experiences.
+
+Today, we continue to serve our customers with the same passion, quality, and warm hospitality that inspired us from the beginning. ☕❤️</b>
+                    </div>
+
+                </div>
+
+                   
             </BodyContainer>
             <Footer/>
        </div>
